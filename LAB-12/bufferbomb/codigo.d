b2d5a803562a3da99a1b7d811bd219e9b01ed28e
Maria (Malu) Luiza <148253498+marialuiza15@users.noreply.github.com>
2025-11-06 13:42:05 +0000

    add: LAB-12

---
+++ LAB-12/bufferbomb/codigo.d
@@ -0,0 +1,9 @@
+
+codigo.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <main>:
+   0:	bf 04 03 02 01       	mov    $0x1020304,%edi
+   5:	c3                   	ret
---
+++ LAB-12/bufferbomb/codigo.s
@@ -0,0 +1,7 @@
+
+.text
+.global main
+.type codigo, @function
+main:
+    movl $0x01020304, %edi
+    ret
